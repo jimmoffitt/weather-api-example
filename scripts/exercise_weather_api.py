6a06a8e247565edc7ba4db631a3b8e929ecb5fec
@snowman <jimmoffitt@yahoo.com>
2023-05-04 18:09:55 -0600

    Update exercise_weather_api.py

--- scripts/exercise_weather_api.py
+++ scripts/exercise_weather_api.py
@@ -8,9 +8,10 @@ tinybird_token = os.environ.get('TINYBIRD_TOKEN')
 
 while True: #Just keep making requests... 
     headers = {"Authorization": f"Bearer {tinybird_token}"}
-    params = {"sensor_type": 'temp', "max_results": 10}
-    params = {}
     #params = {"start_date": start_date, "end_date": end_date, "max_results": max_results, "sensor_type": sensor_type}
+    #params = {"sensor_type": 'temp', "max_results": 10}
+    params = {}
+    
     response = requests.get(tinybird_api_endpoint, headers=headers, params=params)
     print(response.json())
     time.sleep(4)  # wait (seconds) before making the next request
