dff1524cf28928e10b8d7bd0a05846fe143f2e4d
@snowman <jimmoffitt@yahoo.com>
2023-05-04 18:09:07 -0600

    Create exercise_weather_api.py

---
+++ scripts/exercise_weather_api.py
@@ -0,0 +1,16 @@
+import time
+import requests
+from dotenv import load_dotenv
+load_dotenv()
+
+tinybird_api_endpoint = "https://api.tinybird.co/v0/pipes/weather_data.json"
+tinybird_token = os.environ.get('TINYBIRD_TOKEN')
+
+while True: #Just keep making requests... 
+    headers = {"Authorization": f"Bearer {tinybird_token}"}
+    params = {"sensor_type": 'temp', "max_results": 10}
+    params = {}
+    #params = {"start_date": start_date, "end_date": end_date, "max_results": max_results, "sensor_type": sensor_type}
+    response = requests.get(tinybird_api_endpoint, headers=headers, params=params)
+    print(response.json())
+    time.sleep(4)  # wait (seconds) before making the next request
